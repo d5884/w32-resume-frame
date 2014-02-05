422264ecc774897be59ee9310b0c4db4ba08afdf
Daisuke Kobayashi <d5884jp@gmail.com>
2014-02-05 22:08:43 +0900

    Initial commit.

---
+++ w32-resume-frame.el
@@ -0,0 +1,32 @@
+;;; w32-resume-frame.el --- Resume frame size and position for NTEmacs.
+
+;; Copyright (C) 2014  Daisuke Kobayashi
+
+;; Author: Daisuke Kobayashi <d5884jp@gmail.com>
+;; Version: 0.1
+;; Keywords: frames
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+
+(provide 'w32-resume-frame)
+
+;;; w32-resume-frame.el ends here
